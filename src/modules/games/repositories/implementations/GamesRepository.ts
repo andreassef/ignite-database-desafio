a67498c5e6e337a34ceaf9e38c8a4af3f0ca59af
andreassef <andreassef02@gmail.com>
2021-06-15 13:21:17 -0400

    [UPDATE] - Implementacao de consulta em modulo de games

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -7,24 +7,31 @@ import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
+  private userRepo: Repository<User>;
 
   constructor() {
     this.repository = getRepository(Game);
+    this.userRepo = getRepository(User)
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    return await this.repository
+      .createQueryBuilder("games").where("LOWER(games.title) LIKE LOWER(:param)",
+       {param: `%${param}%`})
+       .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT count(*) FROM games;"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const users = await this.repository.createQueryBuilder("games")
+    .leftJoinAndSelect("games.users", "users")
+    .where("games.id = :id", {id})
+    .getOneOrFail();
+    const getUsers = users?.users;
+    return getUsers;
   }
 }
